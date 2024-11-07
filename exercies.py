182ea8e7eb81fb97cf72a32a546b57ed829bad32
g g <gabedotgutierrez@gmail.com>
2024-11-07 15:42:42 -0500

    Rendered with next turn message

--- exercies.py
+++ exercies.py
@@ -12,7 +12,40 @@ class Game():
     def play_game(self):
         print("ITS TIME TO PLAY THE GAMEEEEEE")
 
+    def print_board(self):
+        b = self.board
+        print(f"""
+            A   B   C
+        1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
+            ----------
+        2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
+            ----------
+        3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
+        """)
+
+    def print_message(self):
+    ## If there is a tie: print("Tie game!")
+    ## If there is a winner: print(f"{self.winner} wins the game!")
+    ## Otherwise: print(f"It's player {self.turn}'s turn!")
+    ## guessing
+        if (self.winner == "tie"):
+            print("Tie game!")
+        elif (self.winner == 'X' or self.winner == 'O'):
+            print(f"{self.winner} wins the game!")
+        else:
+            print(f"It's player {self.turn}'s turn!")
+    
+    def render(self):
+    # OPTIONAL
+    # Call upon print_board
+    ## Call upon print_message
+        self.print_board()
+        self.print_message()
+
+
+
 
 # creating a game, then calling the play game method.
 game_instance = Game()
 game_instance.play_game()
+game_instance.render()
